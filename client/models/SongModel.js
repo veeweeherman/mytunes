9b97002a9c75ab3cbc204ad8d830fd62465c34bd
veeweeherman <vy.hoang.cu@gmail.com>
2015-07-16 10:15:15 -0700

    wrote methods in SongModel

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -10,8 +10,18 @@ var SongModel = Backbone.Model.extend({
     console.log('this in song model', this);
     this.trigger('play', this);
     // this.model.test();
+  }
 
-    
+  enqueue: function() {
+  	this.trigger('enqueue',this);
+  }
+
+  dequeue: function() {
+  	this.trigger('dequeue',this);
+  }
+
+  ended: function() {
+  	this.trigger('ended',this);
   }
 
 
